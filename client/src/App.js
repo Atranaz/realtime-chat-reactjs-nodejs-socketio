27718f44e39925cbfe115a93055952092e4f7e59
Zaheer Ahmad <atranaz@live.com>
2019-02-08 02:28:41 +0400

    socket added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import io from "socket.io-client";
 
@@ -12,7 +11,7 @@ class App extends Component {
             message: '',
             messages: []
         };
-    this.socket = io('localhost:5000');
+    this.socket = io();
     this.socket.on('RECEIVE_MESSAGE', function(data){
       addMessage(data);
   });
