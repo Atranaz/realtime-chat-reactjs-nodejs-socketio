b8b8cacab1fcf2bd971c80006ae9408b515eb56b
Zaheer Ahmad <atranaz@live.com>
2019-02-08 01:21:49 +0400

    first commit u

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-
 var socket = require('socket.io');
 
+const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
+// API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
@@ -19,6 +16,14 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
